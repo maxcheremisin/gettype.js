dfd90308c371c28e5df263f92f7bd815ae2225e9
Uche Jude <iamuchejude@gmail.com>
2019-01-10 17:40:36 +0100

    ch: remove toLowerCase() method and update doc

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = (data = null) => 
-  data === undefined ? 'undefined' : data === null ? 'null' : data === NaN ? 'NaN' : Object.getPrototypeOf(data).constructor.name.toLowerCase();
+  data === undefined ? 'undefined' : data === null ? 'null' : data === NaN ? 'NaN' : Object.getPrototypeOf(data).constructor.name;
   
--- test/index.js
+++ test/index.js
@@ -2,23 +2,24 @@ const assert = require('assert');
 const getType = require('../');
 
 describe('getType', () => {
+  class TestClass {}
   let car;
   
   describe('test for strings', () => {
     it ('should return `string`', () => {
-      assert('string', getType('This is a string'));
-      assert('string', getType(''));
-      assert('string', new String(2019));
-      assert('string', String(2019));
+      assert('String', getType('This is a string'));
+      assert('String', getType(''));
+      assert('String', new String(2019));
+      assert('String', String(2019));
     });
   })
 
   describe('get type of number', () => {
     it ('should return `number`', () => {
-      assert('number', getType(1));
-      assert('number', getType(0));
-      assert('number', getType(new Number('2019')));
-      assert('number', getType(Number('2019')));
+      assert('Number', getType(1));
+      assert('Number', getType(0));
+      assert('Number', getType(new Number('2019')));
+      assert('Number', getType(Number('2019')));
     });
   });
 
@@ -32,10 +33,10 @@ describe('getType', () => {
 
   describe('test for arrays', () => {
     it ('should test `array`', () => {
-      assert('array', getType([]));
-      assert('array', getType([2, 3]));
-      assert('array', getType(new Array(2)));
-      assert('array', getType(Array(2)));
+      assert('Array', getType([]));
+      assert('Array', getType([2, 3]));
+      assert('Array', getType(new Array(2)));
+      assert('Array', getType(Array(2)));
     });
   });
 
@@ -50,36 +51,46 @@ describe('getType', () => {
 
   describe('test for functions', () => {
     it ('should return `function`', () => {
-      assert('function', getType(function() {}));
-      assert('function', getType(() => {}));
-      assert('function', getType(new Function('x', 'y', 'x' * 'y')));
+      assert('Function', getType(function() {}));
+      assert('Function', getType(() => {}));
+      assert('Function', getType(new Function('x', 'y', 'x' * 'y')));
     });
   });
 
   describe('test for sets', () => {
     it('should return `set`', () => {
-      assert('set', getType(new Set([1, 2, 3])));
+      assert('Set', getType(new Set([1, 2, 3])));
     });
   })
 
   describe('test for boolean', () => {
     it('should return 1boolean`', () => {
-      assert('boolean', getType(true));
-      assert('boolean', getType(false));
+      assert('Boolean', getType(true));
+      assert('Boolean', getType(false));
     });
   })
 
   describe('test for null', () => {
     it('should return `null`', () => {
-      assert('null', getType(null));
+      assert('Null', getType(null));
     });
   });
 
   describe('test for undefined', () => {
     it('should return `undefined`', () => {
-      assert('undefined', getType());
-      assert('undefined', getType(undefined));
-      assert('undefined', getType(car));
+      assert('Undefined', getType());
+      assert('Undefined', getType(undefined));
+      assert('Undefined', getType(car));
     });
   });
+
+  describe('more tests', () => {
+    it ('should return the right datatype', () => {
+      assert('Date', getType(new Date()));
+      assert('Date', getType(new Date(2018, 7, 2)));
+      assert('TypeError', getType(new TypeError()));
+      assert('RegExp', getType(/regExp/));
+      assert('TextClass', getType(new TestClass()));
+    })
+  })
 });
